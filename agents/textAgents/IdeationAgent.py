6343e0cfb596dd30ecf2e8cbd6f59dab3e6b8815
Jota <joao.airesmatos@gmail.com>
2023-06-12 14:27:24 +0100

    Changed agent

--- agents/textAgents/IdeationAgent.py
+++ agents/textAgents/IdeationAgent.py
@@ -2,26 +2,31 @@ from collections import namedtuple
 from agents.Agent import Agent
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
-from langchain.agents import load_tools, initialize_agent
-from langchain.agents import AgentType
+from langchain.tools import Tool
+from langchain.utilities import GoogleSearchAPIWrapper
 
 main_f = namedtuple('main', ())
 
 class IdeationAgent(Agent):
   def main(self, userPrompt, systemPrompt, config ):
-    llm = ChatOpenAI(temperature=0.0)
-    math_llm = OpenAI(temperature=0.0)
-    tools = load_tools(
-        ["human", "llm-math"], 
-        llm=math_llm,
-    )
+    # llm = ChatOpenAI(temperature=0.0)
+    # tools = load_tools(
+    #     ["human"], 
+    #     llm=llm,
+    # )
 
-    agent_chain = initialize_agent(
-        tools,
-        llm,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True,
-    )
+    # agent_chain = initialize_agent(
+    #     tools,
+    #     llm,
+    #     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    #     verbose=True,
+    # )
+    search = GoogleSearchAPIWrapper()
 
-    answer = agent_chain.run("Give me 3 ideas to create a new twitter thread on the following topic: " + userPrompt + "\n\n")
+    tool = Tool(
+        name = "Google Search",
+        description="Search Google for recent results.",
+        func=search.run
+    )
+    answer = tool.run("Give me 3 ideas to create a new twitter thread on the following topic: " + userPrompt)
     return answer
