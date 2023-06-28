e5deb1b2f583f8ac42d8c81e52f5851cfe975f70
wanda-ai <hi@wanda.so>
2023-06-28 14:12:45 +0100

    Fixed dockerfile and added deploy to ec2 script file

--- tools/deploy-ec2.sh
+++ tools/deploy-ec2.sh
@@ -1,5 +1,5 @@
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker system prune
-sudo docker build -t wandabackend:latest .
-sudo docker run -d --privileged -p 8080:80 --env-file .env wandabackend:latest
+docker build -t wandabackend:latest .
+docker run -d --privileged -p 8080:80 --env-file .env wandabackend:latest
