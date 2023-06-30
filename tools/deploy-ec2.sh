db1b5792efdc4ea104236978d22ac4847e9a1dd3
wanda-ai <hi@wanda.so>
2023-06-30 18:13:23 +0100

    Fix deploy script

--- tools/deploy-ec2.sh
+++ tools/deploy-ec2.sh
@@ -1,5 +1,5 @@
+docker build -t wandabackend:latest .
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
 docker system prune
-docker build -t wandabackend:latest .
 docker run -d --privileged -p 8080:8080 --env-file .env wandabackend:latest
